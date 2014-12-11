0d12b6e15e8bf8bc76b4a449af8dee817cbae5cd
Deborah <deborahc@mit.edu>
2014-12-10 20:25:55 -0500

    debugging accepting a file

--- kerb-server.js
+++ kerb-server.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
+var atob = require('atob');
+var base64 = require('base64-js');
+
+
 
 app.use(express.static(__dirname));
 
@@ -23,37 +27,45 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res){
     console.log('POST /');
-    console.dir(req.body.ticket);
+    console.log(req);
     // add logic to put in the right krbccname
-    var fileName = '/tmp/krb5cc_1000_blah';
+    var fileName = '/tmp/krb5cc_1000';
 
-    var ticket = new Buffer(req.body.ticket, 'base64').toString('ascii');
+    // var uriEncodedString = decodeURIComponent(req.body.ticket);
+
+    // var ticket = atob(req.body.ticket);
+    // var ticket = base64.toByteArray(req.body.ticket);
+
+
+
+    // console.log(ticket);
+    // var ticket = new Buffer(req.body.ticket, 'base64').toString('ascii');
     // var fileName = '/tmp/krb5cc_1000_'+'deborahc';
-    fs.writeFile(fileName, ticket, function(err) {
+    fs.writeFile(fileName, 'hi', function(err) {
     if (err) {
 
         console.log(err);
     } 
     else {
-    	// input to tell remctl where to look
-  		var command = 'sh ' + __dirname + '/kerb.sh ' + "'" + fileName + "'";
+        // input to tell remctl where to look
+        var command = 'sh ' + __dirname + '/kerb.sh ' + "'" + fileName + "'";
 
-		  exec(command, function(error, stdout, stderr) {
-  			// res.send(Buffer.concat(stdout));
-  			res.writeHead(200, {'Content-Type': 'text/html'});
-  			res.write(stdout);
+          exec(command, function(error, stdout, stderr) {
+            // res.send(Buffer.concat(stdout));
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(stdout);
 
-			res.end('bye');
+            res.end('bye');
 
-		    console.log('stdout: ' + stdout);
-		    console.log('stderr: ' + stderr);
-		    if (error !== null) {
-  				res.send(500); // if the script fails, send 500
-		        console.log('exec error: ' + error);
-		    }
-		});
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                res.send(500); // if the script fails, send 500
+                console.log('exec error: ' + error);
+            }
+        });
 
-  		}
+        }
     }); 
 
 });
