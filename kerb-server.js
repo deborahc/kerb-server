7968547f9d08f7f8459b987b08333d64071a5ba8
Deborah <deborahc@mit.edu>
2014-12-11 14:57:24 -0500

    can see principal from ticket

--- kerb-server.js
+++ kerb-server.js
@@ -6,10 +6,13 @@ var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
 var atob = require('atob');
 var base64 = require('base64-js');
+var busboy = require('connect-busboy');
+
 
 
 
 app.use(express.static(__dirname));
+app.use(busboy()); 
 
 
 app.use(bodyParser.urlencoded({
@@ -25,23 +28,67 @@ app.get('/', function(req, res) {
     res.end();
 });
 
+// app.post('/', function(req, res) {
+//     var fstream;
+//     req.pipe(req.busboy);
+//     req.busboy.on('file', function (fieldname, file, filename) {
+//         console.log("Uploading: " + filename); 
+//         fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+//         file.pipe(fstream);
+//         fstream.on('close', function () {
+//             res.redirect('back');
+//         });
+//     });
+// });
+
 app.post('/', function(req, res){
     console.log('POST /');
     console.log(req);
     // add logic to put in the right krbccname
     var fileName = '/tmp/krb5cc_1000';
 
-    // var uriEncodedString = decodeURIComponent(req.body.ticket);
+    var uriEncodedString = decodeURIComponent(req.body.ticket);
 
-    // var ticket = atob(req.body.ticket);
+    var ticket = atob(uriEncodedString);
     // var ticket = base64.toByteArray(req.body.ticket);
+    console.log(req, 'req');
 
+
+    // var fstream;
+    // req.pipe(req.busboy);
+    // req.busboy.on('file', function (fieldname, file, filename) {
+    //     var fileName = '/tmp/krb5cc_1000';
+    //     console.log("Uploading: " + fileName); 
+    //     fstream = fs.createWriteStream();
+    //     file.pipe(fstream);
+    //     fstream.on('close', function () {
+    //         // call the remctl stuff
+
+
+    //         var command = 'sh ' + __dirname + '/kerb.sh ' + "'" + fileName + "'";
+
+    //         exec(command, function(error, stdout, stderr) {
+    //             // res.send(Buffer.concat(stdout));
+    //             res.writeHead(200, {'Content-Type': 'text/html'});
+    //             res.write(stdout);
+
+    //             res.end('bye');
+
+    //             console.log('stdout: ' + stdout);
+    //             console.log('stderr: ' + stderr);
+    //             if (error !== null) {
+    //                 res.send(500); // if the script fails, send 500
+    //                 console.log('exec error: ' + error);
+    //             }
+    //         });
+    //     });
+    // });
 
 
     // console.log(ticket);
     // var ticket = new Buffer(req.body.ticket, 'base64').toString('ascii');
     // var fileName = '/tmp/krb5cc_1000_'+'deborahc';
-    fs.writeFile(fileName, 'hi', function(err) {
+    fs.writeFile(fileName, ticket, function(err) {
     if (err) {
 
         console.log(err);
@@ -51,7 +98,7 @@ app.post('/', function(req, res){
         var command = 'sh ' + __dirname + '/kerb.sh ' + "'" + fileName + "'";
 
           exec(command, function(error, stdout, stderr) {
-            // res.send(Buffer.concat(stdout));
+            // res.send(Buffehtr.concat(stdout));
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(stdout);
 
